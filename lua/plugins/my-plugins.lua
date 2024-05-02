37f3270ff46c9173609e6cfd76a6b4ec4c36f612
danntastico <nicolasdpf@gmail.com>
2024-05-02 11:58:00 -0500

    first Hello Lua

---
+++ lua/plugins/my-plugins.lua
@@ -0,0 +1,32 @@
+local colorscheme = require("lazyvim.plugins.colorscheme")
+return {
+  -- color theme
+  { "sainnhe/sonokai" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "sonokai",
+    },
+  },
+
+  {
+    "chentoast/marks.nvim",
+    config = function()
+      require("marks").setup()
+    end,
+  },
+
+  {
+    "folke/which-key.nvim",
+    opts = function()
+      local wk = require("which-key")
+      wk.register({
+        ["<leader>f"] = { name = "+file" },
+        ["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Find File" },
+        ["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+        ["<leader>fn"] = { "<cmd>enew<cr>", "New File" },
+      })
+    end,
+  },
+}
